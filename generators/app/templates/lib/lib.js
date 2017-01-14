f749bacb15775915834833f8d5a2aa5ccadbe01c
nju33 <nju33.ki@gmail.com>
2017-01-15 02:19:44 +0900

    Fix to replace to ejs template

--- generators/app/templates/lib/lib.js
+++ generators/app/templates/lib/lib.js
@@ -2,7 +2,7 @@ const defaultOpts = {
 };
 export {defaultOpts};
 
-export default function anchor(opts = {}) {
+export default function <%=pluginname%>(opts = {}) {
   opts = Object.assign({}, defaultOpts, opts);
   return $ => {
     return $;
